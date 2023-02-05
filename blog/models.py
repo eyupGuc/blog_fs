5e5ac03eb5810be7ca53a573f44e10b4a3de0c1e
eyupGuc <meydanguresi@gmail.com>
2023-02-05 14:28:40 +0300

    move

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,13 @@ class Blog(models.Model):
     title = models.CharField(max_length=100,unique=True)
     content =models.TextField(blank=True)
     category = models.ForeignKey(Category,on_delete=models.PROTECT)
+    is_published = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
     
     
 
