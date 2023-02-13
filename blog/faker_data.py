14a128426f1a926ed2ebf6e9e9d7ad6482c25164
eyupGuc <meydanguresi@gmail.com>
2023-02-13 17:54:14 +0300

    move

--- blog/faker_data.py
+++ blog/faker_data.py
@@ -1,23 +1,22 @@
 from .models import Category, Blog
 from faker import Faker
-from datetime import datetime
+
 
 
 def run():
-    fake = Faker(['en-US'])
+    fake = Faker(['tr-TR'])
     categories = (
+        "Life",
+        "Science",
         "Politics",
         "Sports",
-        "Millitary",
-        "Travel",
-        "Showbiz"
+
     )
 
-    for category_id in categories:
-        new_category = Category.objects.create(category_name=category_id)
-        for _ in range(30):
-            Blog.objects.create(category_id=new_category, title=fake.company(), content=fake.text(
-            ), status=fake.pybool(), created_date=fake.date_time_this_year(), updated_date=fake.date_time_this_year())
+    for category in categories:
+        new_category = Category.objects.create(name=category)
+        for _ in range(20):
+            Blog.objects.create(category=new_category,
+                                title=fake.name(), content=fake.text())
 
     print('Finished')
-# 1.11 kaldım BE-20 : Project solution@ Henry 02.01.2023
