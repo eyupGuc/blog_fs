99ce175055a089ecdb9ff0a37e3baaa2852a815d
eyupGuc <meydanguresi@gmail.com>
2023-02-14 21:31:53 +0300

    blog view

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
 
 from rest_framework.viewsets import ModelViewSet
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, Blog
+from .serializers import CategorySerializer,BlogSerializer
+
 
 class CategoryView(ModelViewSet):
-    queryset =Category.objects.all()
+    queryset = Category.objects.all()
     serializer_class = CategorySerializer
-        
 
 
+class BlogView(ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
