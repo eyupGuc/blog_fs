4819e9d16e6f02ba52967217af784d433586d4ce
eyupGuc <meydanguresi@gmail.com>
2023-02-14 21:07:04 +0300

    move

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from .models import Category
+from .serializers import CategorySerializer
+
+class CategoryView(ModelViewSet):
+    queryset =Category.objects.all()
+    serializer_class = CategorySerializer
+        
+
+
